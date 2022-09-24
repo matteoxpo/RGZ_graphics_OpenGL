61de7a03f7b22e0f326a70c37998befa557a3b19
Xpomin <s-hromin@mail.ru>
2022-09-25 02:03:49 +0700

    refactoring

--- fractal.cpp
+++ fractal.cpp
@@ -24,14 +24,16 @@ int createNumOfBranches();
 
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 800;
-int RECURS_STOP_COUNTER = 0;
-const int MIN_LENGTH = WINDOW_WIDTH / 100;
-const int MAX_LENGTH = WINDOW_WIDTH / 5;
+const int MIN_LENGTH = WINDOW_HEIGHT / 100;
+const int MAX_LENGTH = WINDOW_HEIGHT / 5;
+
+int RECURS_COUNTER = 0;
+const int RECURS_COUNTER_LIMIT = 30000;
 
 int main(int argc, char *argv[]) {
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
-  glutInitWindowPosition(50, 10);
+  glutInitWindowPosition(100, 100);
   glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
   glutCreateWindow("RGZ");
   glutReshapeFunc(resize);
@@ -70,29 +72,25 @@ void display() {
 }
 
 void drawTree(double x, double y, double angle, double length) {
-  double new_length;
-  double x1, y1;
-  double s, new_angle, i;
+  RECURS_COUNTER++;
 
-  RECURS_STOP_COUNTER++;
-  if (RECURS_STOP_COUNTER >= 25000) return;
-
-  if (length > MIN_LENGTH) {
-    x1 = createXCoordinate(x, length, angle);
-    y1 = createYCoordinate(y, length, angle);
-    s = createLenghtFactor(length);
+  if (length > MIN_LENGTH && RECURS_COUNTER < RECURS_COUNTER_LIMIT) {
+    double x1 = createXCoordinate(x, length, angle);
+    double y1 = createYCoordinate(y, length, angle);
+    double length_factor = createLenghtFactor(length);
+    double new_angle = 0;
 
     if (length > MAX_LENGTH) length = WINDOW_HEIGHT / 8;
 
-    if (length < 40)
+    if (length < WINDOW_HEIGHT / 20)
       drawLeaf(x, y, x1, y1);
     else
       drawTrunk(x, y, x1, y1, length);
 
-    for (i = 0; i < createNumOfBranches(); i++) {
+    for (int i = 0; i < createNumOfBranches(); i++) {
       new_angle = createAngle(angle);
-      x1 = createXCoordinate(x, s, angle);
-      y1 = createYCoordinate(y, s, angle);
+      x1 = createXCoordinate(x, length_factor, angle);
+      y1 = createYCoordinate(y, length_factor, angle);
       drawTree(x1, y1, new_angle, length - 5 - rand() % 30);
     }
   }
@@ -105,14 +103,14 @@ void drawLeaf(double x, double y, double x1, double y1) {
     glColor3d(0.19, 0.4, 0);
   }
 
-  for (double i = 0; i <= 3; i++) {
+  for (int i = 0; i <= 3; i++) {
     drawLine(x + i, y, x1, y1);
   }
 }
 
 void drawTrunk(double x, double y, double x1, double y1, double p) {
   glColor3d(0.5, 0.2, 0.1);
-  for (double i = 0; i <= (floor(p / 6)); i++) {
+  for (int i = 0; i <= (floor(p / 6)); i++) {
     drawLine(x + i - floor(p / 12), y, x1, y1);
   }
 }
